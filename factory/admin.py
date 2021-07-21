9177525df5a57feb19be0c6f41791385200069ef
takashifuruya0 <takashi.furuya.0@gmail.com>
2021-07-21 18:19:52 +0900

    - implemented category hierarchy - implemented Material model

--- factory/admin.py
+++ factory/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from factory.models import Factory, FactoryCategory, Machine, AvailableProcess
+from factory.models import Factory, FactoryCategory, Machine, AvailableProcess, Material
 from django.contrib.auth.models import User
 # Register your models here.
 
@@ -14,7 +14,10 @@ class MachineInline(admin.TabularInline):
 
 class FactoryCategoryAdmin(admin.ModelAdmin):
     search_fields = ("name", )
-    list_display = ("pk", "name", "created_at", "last_updated_at", )
+    list_display = ("pk", "layer", "name", "created_at", "last_updated_at", )
+    readonly_fields = ("layer1", "layer2", "layer3")
+    ordering = ("layer", )
+    list_filter = ("layer", )
 
 
 class MachineAdmin(admin.ModelAdmin):
@@ -25,9 +28,27 @@ class MachineAdmin(admin.ModelAdmin):
 class FactoryAdmin(admin.ModelAdmin):
     search_fields = ("name", )
     autocomplete_fields = ("category",)
-    list_display = ("pk", "name", "created_at", "last_updated_at",)
-    list_filter = ("machine", "machine__processes")
+    list_display = ("pk", "name", "_layer1", "_layer2", "_layer3", "created_at", "last_updated_at",)
+    list_filter = (
+        "machine", "machine__processes",
+        "category",
+        # "category__parent_category",
+        # "category__parent_category__parent_category",
+    )
     inlines = (MachineInline, )
+
+    def _layer1(self, obj):
+        return obj.category.layer1
+
+    def _layer2(self, obj):
+        return obj.category.layer2
+
+    def _layer3(self, obj):
+        return obj.category.layer3
+
+    _layer1.short_description = "大分類"
+    _layer2.short_description = "中分類"
+    _layer3.short_description = "小分類"
 
 
 class AvailableProcessAdmin(admin.ModelAdmin):
@@ -39,3 +60,4 @@ admin.site.register(Factory, FactoryAdmin)
 admin.site.register(FactoryCategory, FactoryCategoryAdmin)
 admin.site.register(Machine, MachineAdmin)
 admin.site.register(AvailableProcess, AvailableProcessAdmin)
+admin.site.register(Material)
--- factory/models.py
+++ factory/models.py
@@ -37,21 +37,84 @@ class BaseModel(models.Model):
 
 
 class FactoryCategory(BaseModel):
+    # CHOICES
+    CHOICES_LAYER = ((1, "1. 大分類"), (2, "2. 中分類"), (3, "3. 小分類"),)
+    # FIELDS
     name = models.CharField("カテゴリ名", max_length=255)
+    layer = models.IntegerField("階層", choices=CHOICES_LAYER, default=1)
+    parent_category = models.ForeignKey(
+        "self", blank=True, null=True,
+        verbose_name="親カテゴリ", related_name="paretnt_category", on_delete=models.CASCADE
+    )
 
     def __str__(self):
-        return self.name
+        if self.layer == 1:
+            return self.name
+        elif self.layer == 2:
+            return "{}/{}".format(self.parent_category.name, self.name)
+        elif self.layer == 3:
+            return "{}/{}/{}".format(
+                self.parent_category.parent_category.name, self.parent_category.name, self.name)
+
+    def save(self, *args, **kwargs):
+        if self.layer == 1 and self.parent_category:
+            raise Exception('Category of Layer1 should not have parent_category')
+        elif self.layer == 2 and (self.parent_category is None or self.parent_category.layer != 1):
+            raise Exception('Category of Layer2 should have parent_category of Layer1')
+        elif self.layer == 3 and (self.parent_category is None or self.parent_category.layer != 2):
+            raise Exception('Category of Layer3 should have parent_category of Layer2')
+        return super(FactoryCategory, self).save()
+
+    @property
+    def layer1(self):
+        if self.layer == 1:
+            return self
+        elif self.layer == 2:
+            return self.parent_category
+        elif self.layer == 3:
+            return self.parent_category.parent_category
+
+    @property
+    def layer2(self):
+        if self.layer == 1:
+            return None
+        elif self.layer == 2:
+            return self
+        elif self.layer == 3:
+            return self.parent_category
+
+    @property
+    def layer3(self):
+        if self.layer == 3:
+            return self
+        else:
+            return None
 
 
 class Factory(BaseModel):
+    # CHOICES
+    CHOICES_PREFECTURE = (
+        (k, k) for k in (
+            "北海道", "青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県",
+            "茨城県", "栃木県", "群馬県", "埼玉県", "千葉県", "東京都", "神奈川県",
+            "新潟県", "富山県", "石川県", "福井県", "山梨県", "長野県", "岐阜県",
+            "静岡県", "愛知県", "三重県", "滋賀県", "京都府", "大阪府", "兵庫県",
+            "奈良県", "和歌山県", "鳥取県", "島根県", "岡山県", "広島県", "山口県",
+            "徳島県", "香川県", "愛媛県", "高知県", "福岡県", "佐賀県", "長崎県",
+            "熊本県", "大分県", "宮崎県", "鹿児島県", "沖縄県", "-"
+        )
+    )
+
+    # fields
     name = models.CharField("工場名", max_length=255)
     detail = models.TextField("工場紹介")
-    prefecture = models.CharField("都道府県", max_length=255)
+    prefecture = models.CharField("都道府県", max_length=255, choices=CHOICES_PREFECTURE)
     address1 = models.CharField("市町村区", max_length=255)
     address2 = models.CharField("番地", max_length=255)
     address3 = models.CharField("建物名", max_length=255, blank=True, null=True)
     category = models.ForeignKey(
-        FactoryCategory, verbose_name="カテゴリ", on_delete=models.DO_NOTHING, blank=True, null=True
+        FactoryCategory, verbose_name="カテゴリ", on_delete=models.DO_NOTHING, blank=True, null=True,
+        limit_choices_to={"layer": 3, "is_active": True},
     )
 
     def __str__(self):
@@ -65,10 +128,44 @@ class AvailableProcess(BaseModel):
         return self.name
 
 
+class Material(BaseModel):
+    name = models.CharField("材質名", max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
 class Machine(BaseModel):
+    CHOICES_ACCURACY = (
+        ("10分台", "10分台"),
+        ("100分台", "100分台"),
+        ("1000分台", "1000分台"),
+    )
     name = models.CharField("機械名", max_length=255)
-    owned_by = models.ForeignKey(Factory, verbose_name="保有工場", on_delete=models.CASCADE)
-    processes = models.ManyToManyField(AvailableProcess, verbose_name="対応加工", blank=True)
+    owned_by = models.ForeignKey(
+        Factory, verbose_name="保有工場", on_delete=models.CASCADE, limit_choices_to={"is_active": True},)
+    processes = models.ManyToManyField(
+        AvailableProcess, blank=True, verbose_name="対応加工", limit_choices_to={"is_active": True},)
+    available_size_height = models.IntegerField("対応加工な製品サイズ（縦）", null=True, blank=True)
+    available_size_width = models.IntegerField("対応加工な製品サイズ（横）", null=True, blank=True)
+    available_size_diagon = models.IntegerField("対応加工な製品サイズ（斜め）", null=True, blank=True)
+    materials = models.ManyToManyField(
+        Material, blank=True, verbose_name="材質", limit_choices_to={"is_active": True},)
+    accuracy = models.CharField("加工精度（上限）", max_length=255, choices=CHOICES_ACCURACY, blank=True, null=True)
+    detail = models.TextField("備考・詳細", blank=True, null=True)
+    processes_other = models.CharField("対応可能（その他）", max_length=255, blank=True, null=True)
+
+#     機械名 自由記述
+#     対応加工 MAリスト
+#     加工可能な製品サイズ
+#     縦 自由記述(数値)
+#     横 自由記述(数値)
+#     斜め 自由記述(数値)
+#     材質 MAリスト
+#     加工精度（上限）        SAリスト
+#     最短製作日数  自由記述(数値)
+#     備考・詳細 自由記述（text）
+#     対応加工（その他）
 
     def __str__(self):
         return self.name
