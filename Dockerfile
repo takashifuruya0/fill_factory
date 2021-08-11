31599bcbba4e0be85da0fcb8c56481e88356a6ff
Takashi Furuya <takashi.furuya.0@gmail.com>
2021-08-11 15:13:10 +0900

    add/modify files for docker

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.9
+ENV PYTHONUNBUFFERED 1
+RUN apt update && apt upgrade -y && apt clean
+ADD requirements.txt requirements.txt
+RUN pip3 install -r requirements.txt
+COPY ./ /home/fill_factory
+WORKDIR /home/fill_factory
--- fill_factory/settings.py
+++ fill_factory/settings.py
@@ -130,6 +130,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
 SITE_ID = 1
 
+# staticのURL
+STATIC_URL = '/static/'
+# project全体のstaticファイル格納場所
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "staticfiles"),
+)
+# collectstatic の格納場所
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
